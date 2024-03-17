b533a87876e1b3823c84fb0686ae8f91426f8504
Pranay Khadatkar <khadatkar1990@gmail.cm>
2024-03-18 01:49:02 +0530

    End of section 3

--- src/AuctionService/Controllers/AuctionsController.cs
+++ src/AuctionService/Controllers/AuctionsController.cs
@@ -2,6 +2,7 @@
 using AuctionService.DTOs;
 using AuctionService.Entities;
 using AutoMapper;
+using AutoMapper.QueryableExtensions;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -21,14 +22,24 @@ namespace AuctionService.Controllers
 		}
 
 		[HttpGet]
-		public async Task<ActionResult<List<AuctionDTO>>> GetAllAuctions()
+		public async Task<ActionResult<List<AuctionDTO>>> GetAllAuctions(string date)
 		{
-			var auctions = await _Context.Auctions
-				.Include(x => x.Item)
-				.OrderBy(x => x.Item.Make)
-				.ToListAsync();
+			var query = _Context.Auctions.OrderBy(x => x.Item.Make).AsQueryable();
 
-			return _Mapper.Map<List<AuctionDTO>>(auctions);
+			if (!string.IsNullOrEmpty(date))
+			{
+				query=query.Where(x => x.UpdatedAt.CompareTo(DateTime.Parse(date).ToUniversalTime()) > 0);	
+			}
+
+
+			//var auctions = await _Context.Auctions
+			//	.Include(x => x.Item)
+			//	.OrderBy(x => x.Item.Make)
+			//	.ToListAsync();
+
+			//return _Mapper.Map<List<AuctionDTO>>(auctions);
+
+			return await query.ProjectTo<AuctionDTO>(_Mapper.ConfigurationProvider).ToListAsync();
 		}
 
 		[HttpGet("{id}")]
---
+++ src/SearchService/Services/AuctionSvcHttpClient.cs
@@ -0,0 +1,28 @@
+﻿using MongoDB.Entities;
+using SearchService.Models;
+
+namespace SearchService.Services
+{
+	public class AuctionSvcHttpClient
+	{
+		private readonly HttpClient _HttpClient;
+		private readonly IConfiguration _Config;
+
+		public AuctionSvcHttpClient(HttpClient httpClient,IConfiguration config)
+        {
+			_HttpClient = httpClient;
+			_Config = config;
+		}
+
+		public async Task<List<Item>> GetItemsForSearchDb()
+		{
+			var lastUpdated = await DB.Find<Item,string>()
+				.Sort(x => x.Descending(a => a.UpdatedAt))
+				.Project(x => x.UpdatedAt.ToString())
+				.ExecuteFirstAsync();
+
+			return await _HttpClient.GetFromJsonAsync<List<Item>>(_Config["AuctionServiceUrl"]
+				+"/api/auctions?date="+lastUpdated);
+		}
+    }
+}

ce34a1ede841898dfe779129e8e16d93bd75ff5b
Pranay Khadatkar <khadatkar1990@gmail.cm>
2025-11-04 21:23:51 +0530

    End of section 5

--- src/AuctionService/Controllers/AuctionsController.cs
+++ src/AuctionService/Controllers/AuctionsController.cs
@@ -5,6 +5,7 @@ using AutoMapper;
 using AutoMapper.QueryableExtensions;
 using Contracts;
 using MassTransit;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -58,13 +59,14 @@ namespace AuctionService.Controllers
 			return _Mapper.Map<AuctionDTO>(auction);
 		}
 
+		[Authorize]
 		[HttpPost]
 		public async Task<ActionResult<AuctionDTO>> CreateAuction(CreateAuctionDTO auctionDTO)
 		{
 			var auction = _Mapper.Map<Auction>(auctionDTO);
-			//TODO: add current user as seller
+			
 
-			auction.Seller = "test";
+			auction.Seller = User.Identity.Name;
 
 			_Context.Auctions.Add(auction);
 
@@ -82,6 +84,7 @@ namespace AuctionService.Controllers
 			return CreatedAtAction(nameof(GetAuctionById), new {auction.Id},newAuction);
 		}
 
+		[Authorize]
 		[HttpPut("{id}")]
 		public async Task<ActionResult> UpdateAuction(Guid id, UpdateAuctionDTO updateAuctionDTO)
 		{
@@ -90,7 +93,8 @@ namespace AuctionService.Controllers
 
 			if (auction == null) return NotFound();
 
-			//TODO: check seller == username
+			if(auction.Seller != User.Identity.Name)
+				return Forbid();
 
 			auction.Item.Make = updateAuctionDTO.Make ?? auction.Item.Make;
 			auction.Item.Model = updateAuctionDTO.Model ?? auction.Item.Model;
@@ -107,6 +111,7 @@ namespace AuctionService.Controllers
 			return BadRequest("Problem saving changes");
 		}
 
+		[Authorize]
 		[HttpDelete("{id}")]
 		public async Task<ActionResult> DeleteAuction(Guid id)
 		{
@@ -114,7 +119,8 @@ namespace AuctionService.Controllers
 
 			if (auction == null) return NotFound();
 
-			//TODO: check seller == username
+			if(auction.Seller != User.Identity.Name)
+				return Forbid();
 
 			_Context.Auctions.Remove(auction);
 
